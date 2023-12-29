58584928c5317e0d753f6774b5b1fe9f768ce53f
Leo-System <tenri123@protonmail.com>
2023-12-29 17:41:28 -0300

    Primeiro commit de um programa com api!

---
+++ Movies-Finder.py
@@ -0,0 +1,39 @@
+import requests
+
+print("""
+############################################################
+NOTE:To use this program you will need to:
+
+1-Make an acount on rapidapi.com;
+2-Subscribe in the api MoviesDatabase(link: https://rapidapi.com/SAdrian/api/moviesdatabase/) on the free plan;
+3-Copy your api key from the site to use on this program, the api key will be on the code snippets near "X-RapidAPI-Key".
+
+############################################################
+""")
+
+url = "https://moviesdatabase.p.rapidapi.com/titles"
+
+key=input("Insert your api key from RapidAPI.com: ")
+ano=int(input("Insert the year of the movies: "))
+genero=input("Insert the genre of the movies you want: ")
+print("\n")
+ano=str(ano)
+
+querystring = {"limit":"50","year":ano,"genre":genero,"titleType":"movie"}
+headers = {
+	"X-RapidAPI-Key": key,
+	"X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+resultados = int(response.json()['entries'])
+
+n=0
+
+while n < resultados:
+    nome = response.json()['results'][n]['titleText']['text']
+    print(nome)
+    n=n+1
+
+print(f"\nNumber of results: {resultados}\n")
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Movies-Finder
 A simple python prgram that uses the api MoviesDatabse from RapidApi.com to search for some movies based on your filters.
+
+**NOTE**:To use this program you will need to:
+1-Make an acount on [RapidApi](https://rapidapi.com/hub);
+2-Subscribe in the api [MoviesDatabase](https://rapidapi.com/SAdrian/api/moviesdatabase/) on the free plan;
+3-Copy your api key from the site to use on this program, the api key will be on the code snippets near to _"X-RapidAPI-Key"_.
+
+After entering your api key on the program it will ask you to enter a movie release data and a movie genere, then the api will send you a list of movies based on your parameters and the number of movies sent. 
+
+This is a simple program that i done just to learn how to use an api in my codes.
